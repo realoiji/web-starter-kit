3037948549d382f344df2af470c4280cd79c738f
Prach <bombom.55@hotmail.com>
2019-04-06 02:27:52 +0700

    seo

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router }  from 'react-router-dom';
 import styled from '@emotion/styled';
+import { Helmet } from "react-helmet";
 
 import Routes from './routes';
 import './styles';
@@ -8,6 +9,11 @@ import './styles';
 const App = () => {
   return (
     <Router>
+      <Helmet>
+        <title>App</title>
+        <meta name="description" content="testing react  web starter kit" />
+        <meta name="keywords" cpntent="react,seo, web starter kit" />
+      </Helmet>
       <Styled>
         <Routes/>
       </Styled>
@@ -18,5 +24,5 @@ const App = () => {
 export default App;
 
 const Styled = styled.div`
-  label: AppRouter;  
+  label: AppRouter;
 `;
